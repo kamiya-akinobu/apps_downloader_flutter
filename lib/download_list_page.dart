54196ca7cd03649ca7ad9a4c3c32a1ab52ed1666
kamiya-akinobu <kamiya-akinobu@dmm.com>
2024-09-06 17:34:25 +0900

    Version Up 1.1.0 - add personal delete process - add file rename process - add info dialog

--- lib/download_list_page.dart
+++ lib/download_list_page.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:app_downloader_flutter/constants.dart';
 import 'package:app_downloader_flutter/download_list_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -23,21 +24,22 @@ class DownloadList extends ConsumerWidget {
         actions: [
           files!.isNotEmpty
               ? IconButton(
+                  icon: const Icon(Icons.delete),
                   onPressed: () {
                     showDialog<void>(
                         context: context,
                         builder: (_) {
                           return AlertDialog(
-                            title: const Text('すべて削除する'),
+                            title: const Text('All Delete'),
                             actions: [
                               GestureDetector(
-                                  child: const Text('いいえ'),
+                                  child: const Text('No'),
                                   onTap: () {
                                     Navigator.pop(context);
                                   }),
                               const SizedBox(width: 16),
                               GestureDetector(
-                                  child: const Text('はい'),
+                                  child: const Text('Yes'),
                                   onTap: () {
                                     notifier.allDelete();
                                     Navigator.pop(context);
@@ -46,9 +48,14 @@ class DownloadList extends ConsumerWidget {
                           );
                         });
                   },
-                  icon: const Icon(Icons.delete),
                 )
-              : Container()
+              : Container(),
+          files!.isNotEmpty
+              ? IconButton(
+                  icon: const Icon(Icons.refresh),
+                  onPressed: () => notifier.refresh,
+                )
+              : Container(),
         ],
       ),
       body: files.isNotEmpty
@@ -57,26 +64,114 @@ class DownloadList extends ConsumerWidget {
               itemCount: files?.length ?? 0,
               itemBuilder: (BuildContext context, int index) {
                 final file = File(files![index].path);
-                return Row(
-                  children: [
-                    Expanded(child: Text(basename(file.path))),
-                    SizedBox(
-                      width: 120,
-                      child: ElevatedButton(
-                        onPressed: () async {
-                          await OpenFile.open(file.path);
-                          file.open();
-                        },
-                        child: const Text('Install'),
+                return GestureDetector(
+                  onTap: () async {
+                    await OpenFile.open(file.path);
+                  },
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              basename(file.path),
+                              style: const TextStyle(fontSize: 16.0),
+                            ),
+                            Text(
+                              file.lastModifiedSync().toString(),
+                              style: const TextStyle(fontSize: 12.0),
+                            ),
+                          ],
+                        ),
                       ),
-                    )
-                  ],
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20.0, vertical: 10.0),
+                        child: _getPopupMenuButton(context, file, notifier),
+                      ),
+                    ],
+                  ),
                 );
               },
             )
           : const Center(
-              child: Text('ダウンロードしたファイルはありません'),
+              child: Text('No Downloaded files'),
             ),
+    );
+  }
+
+  Widget _getPopupMenuButton(
+      BuildContext context, File file, DownloadListNotifier notifier) {
+    TextEditingController controller = TextEditingController();
+    return PopupMenuButton(
+      position: PopupMenuPosition.under,
+      child: const Icon(Icons.more_vert),
+      itemBuilder: (_) {
+        return [
+          PopupMenuItem<String>(
+              child: const ListTile(
+                title: Text('Rename'),
+              ),
+              onTap: () {
+                controller.text = basename(file.path);
+                showDialog<void>(
+                  context: context,
+                  builder: (_) {
+                    return AlertDialog(
+                      title: const Text('Rename'),
+                      content: TextField(
+                        controller: controller,
+                      ),
+                      actions: [
+                        GestureDetector(
+                          child: const Text('Cancel'),
+                          onTap: () => Navigator.pop(context),
+                        ),
+                        GestureDetector(
+                          child: const Text('Modify'),
+                          onTap: () {
+                            notifier.rename(file, controller.text);
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                );
+              }),
+          PopupMenuItem<String>(
+            child: const ListTile(
+              title: Text('Delete'),
+            ),
+            onTap: () {
+              showDialog<void>(
+                  context: context,
+                  builder: (_) {
+                    return AlertDialog(
+                      title: const Text('Delete Confirmation'),
+                      content: Text(
+                          '${basename(file.path)} ${Constants.newLine} is Delete OK?'),
+                      actions: [
+                        GestureDetector(
+                            child: const Text('No'),
+                            onTap: () {
+                              Navigator.pop(context);
+                            }),
+                        const SizedBox(width: 16),
+                        GestureDetector(
+                            child: const Text('Yes'),
+                            onTap: () {
+                              notifier.delete(file);
+                              Navigator.pop(context);
+                            })
+                      ],
+                    );
+                  });
+            },
+          ),
+        ];
+      },
     );
   }
 }
